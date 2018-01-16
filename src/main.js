f9eb55a29217ea2f4e99de068acc1796a70007d7
gaogao3 <1203582192@qq.com>
2018-01-16 23:30:04 +0800

    可以请求本地数据-express

--- src/main.js
+++ src/main.js
@@ -1,33 +1,31 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
 import VueResource from 'vue-resource';
-import App from './App';
-import goods from 'components/goods/goods';
-import ratings from 'components/ratings/ratings';
-import seller from 'components/seller/seller';
 
-import 'common/stylus/index.styl';
-
-Vue.use(VueRouter);
 Vue.use(VueResource);
 
-let app = Vue.extend(App);
+Vue.config.productionTip = false
 
-let router = new VueRouter({
-	linkActiveClass: 'active'
-});
-
-router.map({
-	'/goods': {
-		component: goods
-	},
-	'/ratings': {
-		component: ratings
-	},
-	'seller': {
-		component: seller
-	}
-});
-
-router.start(app, '#app');
-// router.go('/goods');
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  created(){
+    this.$http.get('./api/seller').then(function(res){
+      res = res.body;
+      console.log(res)
+    });
+    this.$http.get('./api/goods').then(function(res){
+      res = res.body;
+      console.log(res)
+    });
+    this.$http.get('./api/ratings').then(function(res){
+      res = res.body;
+      console.log(res)
+    })
+  }
+})
